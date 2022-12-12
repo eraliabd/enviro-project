15656e98aa40307bf96cd229a556875b379dff1e
eraliabdinazarov <eraliabdinazarov22@gmail.com>
2022-12-12 18:50:52 +0500

    commit and push enviro files

--- config/settings.py
+++ config/settings.py
@@ -160,7 +160,7 @@ MODELTRANSLATION_DEFAULT_LANGUAGE = 'uz'
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
